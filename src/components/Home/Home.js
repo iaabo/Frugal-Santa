59fad5e5b89ef0c04f7a1179cc62801937cd6431
iaabo <insbarroso@gmail.com>
2020-11-29 16:20:21 +0000

    Homepage done but not responsive yet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <NavBar />
+      <NavBar />
+      <div className="img-container">
+        <div className="background-container">
+          <h1 className="header-message">
+            Gifts for all wallets! Say Hello to the Frugal Santa. Choose your
+            budget and see the cool gifts we selected for you. Impress your
+            Secret Santa!
+          </h1>
+        </div>
       </div>
-      Home
+      <div className="budget-question">
+        <h2> How much is your budget?</h2>
+        <div className="buttons">
+          <button className="button-menu">
+            <Link to="/gifts10">Under 10€</Link>
+          </button>
+          <button className="button-menu">
+            <Link to="/gifts20">10€-20€</Link>
+          </button>
+          <button className="button-menu">
+            <Link to="gifts50">20€-50€</Link>
+          </button>
+        </div>
+      </div>{" "}
     </div>
   );
 };
