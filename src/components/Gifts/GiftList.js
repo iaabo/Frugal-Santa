1dd4af65edaf0fb786590fa3f476d7a4493c6f41
iaabo <insbarroso@gmail.com>
2020-11-30 23:07:11 +0000

    minor changes

--- src/components/Gifts/GiftList.js
+++ src/components/Gifts/GiftList.js
@@ -13,7 +13,6 @@ class GiftList extends React.Component {
         description:
           "The Sheep that smiles all year even when you are sad. Anti-alergic. Adapted for children above 14 months",
         category: "kids",
-        isFavorite: false,
         image:
           "https://images.pexels.com/photos/12211/pexels-photo-12211.jpeg?cs=srgb&dl=pexels-tetyana-kovyrina-12211.jpg&fm=jpg",
         url: "https://www.linkedin.com/in/inesbarroso/",
@@ -24,7 +23,6 @@ class GiftList extends React.Component {
         price: 4,
         description: "A classic cube that will develop young minds",
         category: "kids",
-        isFavorite: false,
         image:
           "https://images.pexels.com/photos/19677/pexels-photo.jpg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-19677.jpg&fm=jpg",
         url: "https://www.linkedin.com/in/inesbarroso/",
@@ -36,7 +34,6 @@ class GiftList extends React.Component {
         price: 9,
         description: "Drinking coffee with style. Adapted to microwaves.",
         category: "woman",
-        isFavorite: false,
         image:
           "https://images.pexels.com/photos/1755215/pexels-photo-1755215.jpeg?cs=srgb&dl=pexels-lisa-fotios-1755215.jpg&fm=jpg",
         url: "https://www.linkedin.com/in/inesbarroso/",
@@ -48,7 +45,6 @@ class GiftList extends React.Component {
         price: 9,
         description: "Ergonomic Wine Opener.",
         category: "man",
-        isFavorite: false,
         image:
           "https://images.pexels.com/photos/404341/wine-cork-corkscrew-uncork-404341.jpeg?cs=srgb&dl=pexels-skitterphoto-404341.jpg&fm=jpg",
         url: "https://www.linkedin.com/in/inesbarroso/",
@@ -59,7 +55,6 @@ class GiftList extends React.Component {
         price: 8.79,
         description: "A4 hardcover agenda for 2021.",
         category: "woman",
-        isFavorite: false,
         image:
           "https://images.pexels.com/photos/1765033/pexels-photo-1765033.jpeg?cs=srgb&dl=pexels-ann-h-1765033.jpg&fm=jpg",
         url: "https://www.linkedin.com/in/inesbarroso/",
@@ -70,7 +65,6 @@ class GiftList extends React.Component {
         price: 9,
         description: "A scent of Pine Forest.",
         category: "woman",
-        isFavorite: false,
         image:
           "https://images.pexels.com/photos/3270223/pexels-photo-3270223.jpeg?cs=srgb&dl=pexels-kristina-paukshtite-3270223.jpg&fm=jpg",
         url: "https://www.linkedin.com/in/inesbarroso/",
@@ -81,7 +75,6 @@ class GiftList extends React.Component {
         price: 7.89,
         description: "Black reusable water bottle. Free BPA.",
         category: "man",
-        isFavorite: false,
         image:
           "https://images.pexels.com/photos/3738062/pexels-photo-3738062.jpeg?cs=srgb&dl=pexels-cottonbro-3738062.jpg&fm=jpg",
         url: "https://www.linkedin.com/in/inesbarroso/",
