632b65f268c3d824eed5eb989edf17bb3370ba07
iaabo <insbarroso@gmail.com>
2020-12-26 18:47:56 +0000

    change the about us text and images- without css

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,31 +1,33 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import local from "./local.jpg";
+import local2 from "./local2.jpg";
 
 const AboutUs = () => {
   return (
     <div>
       <NavBar />
       <div className="img-container-about">
-        <div className="background-container">
-          <p className="header-message">
-            Impulse the traditional commerce. Small and family own stores than
-            don’t have a big online presence but have the best gifts for your
-            budget and a lot of love to share!
-          </p>
-        </div>
+        <div className="background-container"></div>
         <div className="questions">
-          <h1 className="question1">
-            Do you know a family that owns a store with cool gifts? Contact us!
-            with alert. Put here a cool form.
-          </h1>
-          <h1 className="question2">
-            Do you need support? Speak directly with Oscar. Put here the image
-            of Oscar with alert.
-          </h1>
+          <p className="question1">
+            This is a fictional web app, however hopefully you can be inspired to buy
+            locally! Buy in small and local stores than don’t have a big online
+            presence but have the best gifts for your budget and a lot of love
+            to share!
+          </p>
+          <p className="question1">
+            There are amazing stores in your neighbourhood that need your support!
+            Buy locally!
+          </p>
+          <div className="pictures">
+            <img src={local} alt="" />
+            <img src={local2} alt="" />
+          </div>
         </div>
-        <Footer />
       </div>
+      <Footer />
     </div>
   );
 };
