7d2b71bc4b36902591aa59d09a9d37eec600f2c3
iaabo <insbarroso@gmail.com>
2020-12-27 00:10:49 +0000

    delete images from about-us and make container for the text

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,8 +1,7 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
-import local from "./local.jpg";
-import local2 from "./local2.jpg";
+import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
@@ -10,21 +9,17 @@ const AboutUs = () => {
       <NavBar />
       <div className="img-container-about">
         <div className="background-container"></div>
-        <div className="questions">
-          <p className="question1">
-            This is a fictional web app, however hopefully you can be inspired to buy
-            locally! Buy in small and local stores than don’t have a big online
-            presence but have the best gifts for your budget and a lot of love
-            to share!
+        <div className="message">
+          <p className="message-h">
+            This is a fictional web app, however hopefully you can be inspired
+            to buy locally! Buy in small and local stores than don’t have a big
+            online presence but have the best gifts for your budget and a lot of
+            love to share! <br></br>
+            <span>
+              There are amazing stores in your neighbourhood that need your
+              support! Buy local!
+            </span>
           </p>
-          <p className="question1">
-            There are amazing stores in your neighbourhood that need your support!
-            Buy local!
-          </p>
-          <div className="pictures">
-            <img src={local} alt="" />
-            <img src={local2} alt="" />
-          </div>
         </div>
       </div>
       <Footer />
